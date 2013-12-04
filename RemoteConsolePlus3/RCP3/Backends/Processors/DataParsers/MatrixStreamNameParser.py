c1ed11e0c3cae541c3a49a5fdc94d1c12625e03d
Dmytro Konobrytskyi <aksonnn@gmail.com>
2013-12-04 17:08:10 -0500

    Added stream name parser for matrix printer

---
+++ RemoteConsolePlus3/RCP3/Backends/Processors/DataParsers/MatrixStreamNameParser.py
@@ -0,0 +1,58 @@
+#Created by Dmytro Konobrytskyi, 2013 (github.com/Akson)
+import re
+
+class Backend(object):
+    def __init__(self, parentNode):
+        self._parentNode = parentNode
+
+    def Delete(self):
+        """
+        This method is called when a parent node is deleted.
+        """
+        pass
+    
+    def GetParameters(self):
+        """
+        Returns a dictionary with object parameters, their values, 
+        limits and ways to change them.
+        """
+        return {}
+    
+    def SetParameters(self, parameters):
+        """
+        Gets a dictionary with parameter values and
+        update object parameters accordingly
+        """
+        pass
+    
+    def ProcessMessage(self, message):
+        """
+        This message is called when a new message comes. 
+        If an incoming message should be processed by following nodes, the 
+        'self._parentNode.SendMessage(message)'
+        should be called with an appropriate message.
+        """
+        
+        processedMessage = dict(message)
+        
+        stream = message["Stream"]
+        if stream.find("MatrixPrinter") == 0:
+            components = stream.split("/")
+            
+            if len(components)==2:
+                formatDescriptionString = components[-1]
+                #find the last digit and split stream name around it
+                reResult = re.search("\d", formatDescriptionString[::-1])
+                p = len(formatDescriptionString) - reResult.start() 
+                
+                processedMessage["Info"]["Dimensions"] = formatDescriptionString[:p] 
+                processedMessage["Info"]["BinaryDataFormat"] = formatDescriptionString[p:] 
+        
+        self._parentNode.SendMessage(processedMessage)
+        
+    def AppendContextMenuItems(self, menu):
+        """
+        Append backend specific menu items to a context menu that user will see
+        when he clicks on a node.
+        """
+        pass
