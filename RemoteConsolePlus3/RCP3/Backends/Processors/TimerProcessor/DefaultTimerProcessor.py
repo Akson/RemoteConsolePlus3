55aa916708a1fdafdbd9cb50410f5afceb9c72c8
Dmytro Konobrytskyi <aksonnn@gmail.com>
2014-03-21 14:31:13 -0400

    New timer output support

--- RemoteConsolePlus3/RCP3/Backends/Processors/TimerProcessor/DefaultTimerProcessor.py
+++ RemoteConsolePlus3/RCP3/Backends/Processors/TimerProcessor/DefaultTimerProcessor.py
@@ -1,27 +1,22 @@
 #Created by Dmytro Konobrytskyi, 2014 (github.com/Akson)
+import math
 
-class EventData(object):
-    lastValueMs = 0
+class TimerData(object):
     def __init__(self):
-        pass
-    
-    def UpdateValueAndReturnDelta(self, value):
+        self.lastValueMs = 0
+
+    def UpdateEventValueAndReturnDelta(self, eventName, value):
         oldVal = self.lastValueMs
         self.lastValueMs = value
         return value-oldVal
-
-class TimerData(object):
-    events = {}
-    def __init__(self):
-        pass
 
 timersStorage = {}
 
 def Ms2Html(ms):
     if ms < 10000:
-        timeStr = "{0:> 8.3f}ms".format(ms)
+        timeStr = "{0:>.3f}ms".format(ms)
     else:
-        timeStr = "{0:> 8.3f}s".format(ms/1000.0)
+        timeStr = "{0:>.3f}s".format(ms/1000.0)
     return timeStr.replace(" ", "&nbsp;")
     
 
@@ -29,22 +24,21 @@ def ParseMessage(message):
     processedMessage = dict()
     processedMessage["Stream"] = message["Stream"]
     processedMessage["Info"] = message["Info"]
-    
-    timerName = message["Info"]["TimerName"]
-    if timerName not in timersStorage:
-        timersStorage[timerName] = TimerData()   
-    timerData = timersStorage[timerName]
-    
-    eventName = message["Info"]["EventName"]
-    if eventName not in timerData.events:
-        timerData.events[eventName] = EventData()
-    eventData = timerData.events[eventName]
-    
-    timeMs = message["Data"]
-    print eventData.lastValueMs
-    deltaMs = eventData.UpdateValueAndReturnDelta(timeMs)
-    print eventData.lastValueMs
-    
-    processedMessage["Data"] = "[Timer: {0}] {1} &Delta;={2} ({3})".format(timerName, Ms2Html(timeMs), Ms2Html(deltaMs), eventName)
 
+    timerData = message["Data"]
+
+    if len(timerData["Events"]) > 1:
+        html = '<table border="1" style="display:inline-block">'
+        html+= '<caption>{0} ({1})</caption>'.format(timerData["Timer name"], Ms2Html(timerData["Total time"]))
+        lastTimeMs = 0
+        for event in timerData["Events"]:
+            html += '<tr><td>{0}</td><td align="right">{1}</td><td align="right">{2}</td></tr>'.format(event["Name"], Ms2Html(event["Time"]), Ms2Html(event["Time"]-lastTimeMs))
+            lastTimeMs = event["Time"] 
+        html += '</table>'
+        
+    else:
+        event = timerData["Events"][0]
+        html = "[TIMER {0}] {1} <b>{2}</b>".format(timerData["Timer name"], event["Name"], Ms2Html(event["Time"]))
+    
+    processedMessage["Data"] = html
     return processedMessage
